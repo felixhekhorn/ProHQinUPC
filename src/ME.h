91fcd4e1900d233fca9de316560b4ee94de38ac0
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2022-06-10 15:55:35 +0200

    Make more matrix elements available

--- src/ME.h
+++ src/ME.h
@@ -5,15 +5,49 @@ namespace ME {
 // cast MMa as macro
 #define Power(x,y) pow(x,y)
 
-// Mandelstam remappings
-#define DEF_u1 cdbl u1 = - s - t1;
+// Mandelstam remapping
+#define DEF_s5 cdbl s5 = s + tp + up;
+#define DEF_u7 cdbl u7 = - s - u1 - up;
 
 /**
- * @brief LO QED matrix element
+ * @brief LO photon-gluon matrix element
  * @param m2 mass
  * @param s center-of-mass energy
  * @param t1 Mandelstam for scattering angle
  */
 cdbl BQED(cdbl m2, cdbl s, cdbl t1);
+
+/**
+ * @brief NLO photon-singlet matrix element
+ * @param m2 mass
+ * @param s center-of-mass energy
+ * @param t1 Mandelstam t1
+ * @param u1 Mandelstam u1
+ * @param tp Mandelstam tp
+ * @param up Mandelstam up
+ */
+cdbl A1(cdbl m2, cdbl s, cdbl t1, cdbl u1, cdbl tp, cdbl up);
+
+/**
+ * @brief NLO photon-non-singlet matrix element
+ * @param m2 mass
+ * @param s center-of-mass energy
+ * @param t1 Mandelstam t1
+ * @param u1 Mandelstam u1
+ * @param tp Mandelstam tp
+ * @param up Mandelstam up
+ */
+cdbl A2(cdbl m2, cdbl s, cdbl t1, cdbl u1, cdbl tp, cdbl up);
+
+/**
+ * @brief NLO interference between photon-singlet and photon-non-singlet matrix element
+ * @param m2 mass
+ * @param s center-of-mass energy
+ * @param t1 Mandelstam t1
+ * @param u1 Mandelstam u1
+ * @param tp Mandelstam tp
+ * @param up Mandelstam up
+ */
+cdbl A3(cdbl m2, cdbl s, cdbl t1, cdbl u1, cdbl tp, cdbl up);
 
 } // namespace ME
---
+++ src/ME/A1.cpp
@@ -0,0 +1,6 @@
+#include "../ME.h"
+
+cdbl ME::A1(cdbl m2, cdbl s, cdbl t1, cdbl u1, cdbl tp, cdbl up) {
+    DEF_u7
+    return (2*Power(tp,2)*(s + u1 + u7))/(u1*u7) - (4*m2*(s + u1 + u7)*Power(s*u7 + t1*(u1 + u7),2))/(tp*Power(u1,2)*Power(u7,2)) - (2*tp*(s + u1 + u7)*(u1*u7*(-s - 2*t1 + u7) + m2*(Power(u1,2) + Power(u7,2))))/(Power(u1,2)*Power(u7,2)) + ((s + u1 + u7)*(-4*Power(m2,2)*Power(u1 + u7,2) - 4*m2*(u1 + u7)*(t1*u1 + (s + t1 + u1)*u7) + u1*u7*(2*Power(s,2) + 4*Power(t1,2) + 2*t1*u1 + Power(u1,2) + 2*s*(2*t1 + u1) - 2*t1*u7 + Power(u7,2))))/(Power(u1,2)*Power(u7,2));
+}
